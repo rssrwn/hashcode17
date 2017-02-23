0f5d872b2ec03933870f8f6ad750d5857a51e580
Rishi <rishi.rabheru16@imperial.ac.uk>
2017-02-23 20:54:26 +0000

    JP

--- Cache/src/Endpoint.java
+++ Cache/src/Endpoint.java
@@ -1,11 +1,12 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Endpoint {
 
     private Map<Video, Integer> videoRequests;
     private Map<Cache, Integer> latencyToCache;
     private final int latencyToDataCenter;
+    public List<Cache> listofcaches = new ArrayList<>();
     private final int id;
 
     public Endpoint(int id, int latencyToDataCenter) {
@@ -21,9 +22,26 @@ public class Endpoint {
 
     public void addCache(Cache cache, Integer latency){
         latencyToCache.put(cache, latency);
+        listofcaches.add(cache);
     }
 
     public int getId() {
         return id;
     }
+
+
+
+
+    public List<Video> listofcandidatevideos() {
+
+        List<Video> vs = videoRequests.entrySet().stream().sorted(Map.Entry.comparingByValue()).map(e -> e.getKey()).collect(Collectors.toList());
+        Collections.reverse(vs);
+        return vs;
+    }
+
+    public List<Cache> listofcacheinascendingorderoflatency() {
+        List<Cache> returnlist = latencyToCache.entrySet().stream().sorted(Map.Entry.comparingByValue()).map(e -> e.getKey()).collect(Collectors.toList());
+        return returnlist;
+    }
+
 }
